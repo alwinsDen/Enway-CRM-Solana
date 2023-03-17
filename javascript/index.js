827fb70e04e8a308a0a1373b8764851738ef77d0
Alwin T <75517758+alwinsDen@users.noreply.github.com>
2023-03-17 12:24:15 +0530

    Updates smart contract to match up with Review saving

--- javascript/index.js
+++ javascript/index.js
@@ -1,34 +1,43 @@
-//program_id HZ3xkJisWVdfLGKidym566bDiAPbyogqr9pWi2yoQh8p
-const dotenv = require("dotenv");
-const web3 = require("@solana/web3.js");
 
-async function main() {
-    let web3 = require("@solana/web3.js");
-    let dotenv = require("dotenv")
-    dotenv.config();
-    const secret = JSON.parse(process.env.PRIVATE_KEY);
-    const secret_key = Uint8Array.from(secret);
-    // this is the generated keypair
-    const publicKeyFromSecret = web3.Keypair.fromSecretKey(secret_key);
-    // program id (created after uplodaing the program)
-    const programId = new web3.PublicKey("9jXP33ikjYLzqnucmwT2X4syGTnxxoqTiHR9ecEJwZdo")
-    const connection = new web3.Connection("http://localhost:8899");
-    // transaction
-    const transaction = new web3.Transaction();
-    const instruction = new web3.TransactionInstruction({
-        keys: [],
-        programId
-    })
-    transaction.add(instruction);
-    //generate transaction signature
-    const transactionSign = await web3.sendAndConfirmTransaction(
-        connection,
-        transaction,
-        [publicKeyFromSecret]
-    );
-    console.log(
-        `Transaction: https://explorer.solana.com/tx/${transactionSign}?cluster=custom`
-    );
-}
 
-main()
+
+
+
+
+
+
+// ==========CREATING HELLO WORLD WITH SOLANA - Part 1============
+// //program_id HZ3xkJisWVdfLGKidym566bDiAPbyogqr9pWi2yoQh8p
+// const dotenv = require("dotenv");
+// const web3 = require("@solana/web3.js");
+//
+// async function main() {
+//     let web3 = require("@solana/web3.js");
+//     let dotenv = require("dotenv")
+//     dotenv.config();
+//     const secret = JSON.parse(process.env.PRIVATE_KEY);
+//     const secret_key = Uint8Array.from(secret);
+//     // this is the generated keypair
+//     const publicKeyFromSecret = web3.Keypair.fromSecretKey(secret_key);
+//     // program id (created after uplodaing the program)
+//     const programId = new web3.PublicKey("9jXP33ikjYLzqnucmwT2X4syGTnxxoqTiHR9ecEJwZdo")
+//     const connection = new web3.Connection("http://localhost:8899");
+//     // transaction
+//     const transaction = new web3.Transaction();
+//     const instruction = new web3.TransactionInstruction({
+//         keys: [],
+//         programId
+//     })
+//     transaction.add(instruction);
+//     //generate transaction signature
+//     const transactionSign = await web3.sendAndConfirmTransaction(
+//         connection,
+//         transaction,
+//         [publicKeyFromSecret]
+//     );
+//     console.log(
+//         `Transaction: https://explorer.solana.com/tx/${transactionSign}?cluster=custom`
+//     );
+// }
+//
+// main()
--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,97 @@
+use borsh::{BorshDeserialize};
 use solana_program::{
     account_info::AccountInfo,
     entrypoint,
     entrypoint::ProgramResult,
     pubkey::Pubkey,
-    msg
+    program_error::ProgramError,
+    msg,
 };
+
+// define the enum for instruction data
+pub enum MovieInstruction {
+    AddMovieReview {
+        title: String,
+        rating: u8,
+        description: String,
+    }
+}
+
+// create an impl off of MovieInstruction that parses the u8 instruction datatype
+impl MovieInstruction {
+    pub fn unpack(input: &[u8]) -> Result<Self, ProgramError> {
+        //split the first byte of the data
+        let (&varient, rest) = input.split_first().ok_or(ProgramError::InvalidInstructionData)
+            .unwrap();
+        // `try_from_slice` is one of the implementations from the BorshDeserialization trait
+        let payload = MovieReviewPayload::try_from_slice(rest).unwrap();
+        // match the first byte and return the movie review sturct
+        Ok(match varient {
+            0 => Self::AddMovieReview {
+                description: payload.description,
+                title: payload.title,
+                rating: payload.rating,
+            },
+            _ => return Err(ProgramError::InvalidInstructionData)
+        })
+    }
+}
+
+// function to log and add it to the blockchain accounts the incoming movie data
+pub fn add_movie_review(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    title: String,
+    rating: u8,
+    description: String,
+) -> ProgramResult {
+    msg!("Adding the movie to blockchain");
+    msg!("Title: {}",title);
+    msg!("Rating: {}", rating);
+    msg!("Description {}", description);
+    Ok(())
+}
+
+// here is the review payload
+#[derive(BorshDeserialize)]
+struct MovieReviewPayload {
+    title: String,
+    rating: u8,
+    description: String,
+}
+
+// hers is the entrypoint!
+entrypoint!(process_instruction);
 pub fn process_instruction(
     program_id: &Pubkey,
     accounts: &[AccountInfo],
-    instruction_data: &[u8]
-)-> ProgramResult {
-    msg!("Hello world! {:?}",program_id);
-    return Ok(());
+    instruction_data: &[u8],
+) -> ProgramResult {
+    let instruction = MovieInstruction::unpack(instruction_data)
+        .unwrap();
+    // matching the outgoing instruction variant
+    match instruction {
+        MovieInstruction::AddMovieReview {description,title,rating}=> {
+            add_movie_review(program_id,accounts,title,rating,description)
+        }
+    }
 }
-entrypoint!(process_instruction);
+
+
+// ==========CREATING HELLO WORLD WITH SOLANA - Part 1============
+// use solana_program::{
+//     account_info::AccountInfo,
+//     entrypoint,
+//     entrypoint::ProgramResult,
+//     pubkey::Pubkey,
+//     msg
+// };
+// pub fn process_instruction(
+//     program_id: &Pubkey,
+//     accounts: &[AccountInfo],
+//     instruction_data: &[u8]
+// )-> ProgramResult {
+//     msg!("Hello world! {:?}",program_id);
+//     return Ok(());
+// }
+// entrypoint!(process_instruction);
