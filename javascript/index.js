00a42391741a58febe91f9a7018eb9cc09aa4ecb
alwinsDen <jsmadesimple@gmail.com>
2023-03-29 08:25:21 +0530

    feat(retrieval): Added data retrieval and loggin

--- javascript/index.js
+++ javascript/index.js
@@ -1,117 +1,129 @@
-// ==========Movie review WITH SOLANA - Part 2============
 let web3 = require("@solana/web3.js");
 let borsh = require("@project-serum/borsh");
 let dotenv = require("dotenv");
+let bs58 = require('bs58')
+
 dotenv.config();
 
 async function main() {
-  const secret = JSON.parse(process.env.PRIVATE_KEY);
-  const secret_key = Uint8Array.from(secret);
-  const publicUserKey = web3.Keypair.fromSecretKey(secret_key);
-  //smart contract program id
-  const program_id = new web3.PublicKey(
-    "9jXP33ikjYLzqnucmwT2X4syGTnxxoqTiHR9ecEJwZdo"
-  );
-  const connection = new web3.Connection("http://localhost:8899");
-  // create a borsh struct constant
-  const movieInstructionLayout = borsh.struct([
-    borsh.u8("variant"),
-    borsh.str("title"),
-    borsh.u8("rating"),
-    borsh.str("description"),
-  ]);
-  // send the review to the contract deployed on the blockchain
-  let buffer = Buffer.alloc(100);
-  let movieName = `BraveHeart${Math.random() * 1000}`
-  //  encode the data
-  movieInstructionLayout.encode(
-    {
-      variant: 0,
-      title: movieName,
-      rating: 5,
-      description: "A great movie",
-    },
-    buffer
-  );
-  buffer = buffer.slice(0, movieInstructionLayout.getSpan(buffer));
-  const [pda] = await web3.PublicKey.findProgramAddress(
-    [publicUserKey.publicKey.toBuffer(), Buffer.from(movieName)],
-    program_id
-  );
-  // logging the PDA
-  console.log(pda.toBase58());
-  // transaction process
-  const transaction = new web3.Transaction();
-  const instruction = new web3.TransactionInstruction({
-    programId: program_id,
-    data: buffer,
-    keys: [
-      {
-        pubkey: publicUserKey.publicKey,
-        isSigner: true,
-        isWritable: false,
-      },
-      {
-        pubkey: pda,
-        isSigner: false,
-        isWritable: true,
-      },
-      {
-        pubkey: web3.SystemProgram.programId,
-        isSigner: false,
-        isWritable: false,
-      },
-    ],
-  });
-  transaction.add(instruction);
-  const transactionSign = await web3.sendAndConfirmTransaction(
-    connection,
-    transaction,
-    [publicUserKey]
-  );
-  console.log(
-    `Transaction: https://explorer.solana.com/tx/${transactionSign}?cluster=custom`
-  );
+    const secret = JSON.parse(process.env.PRIVATE_KEY);
+    const secret_key = Uint8Array.from(secret);
+    const publicUserKey = web3.Keypair.fromSecretKey(secret_key);
+    //smart contract program id
+    const program_id = new web3.PublicKey(process.env.PROGRAM_KEY);
+    const connection = new web3.Connection("http://localhost:8899");
+    // create a borsh struct constant
+    const movieInstructionLayout = borsh.struct([
+        borsh.u8("variant"),
+        borsh.str("title"),
+        borsh.u8("rating"),
+        borsh.str("description"),
+    ]);
+    // send the review to the contract deployed on the blockchain
+    let buffer = Buffer.alloc(100);
+    let movieName = `BraveHeart${Math.random() * 1000}`
+    //  encode the data
+    movieInstructionLayout.encode(
+        {
+            variant: 0,
+            title: movieName,
+            rating: 5,
+            description: "A great movie",
+        },
+        buffer
+    );
+    buffer = buffer.slice(0, movieInstructionLayout.getSpan(buffer));
+    const [pda] = await web3.PublicKey.findProgramAddress(
+        [publicUserKey.publicKey.toBuffer(), Buffer.from(movieName)],
+        program_id
+    );
+    // logging the PDA
+    console.log(pda.toBase58());
+    // transaction process
+    const transaction = new web3.Transaction();
+    const instruction = new web3.TransactionInstruction({
+        programId: program_id,
+        data: buffer,
+        keys: [
+            {
+                pubkey: publicUserKey.publicKey,
+                isSigner: true,
+                isWritable: false,
+            },
+            {
+                pubkey: pda,
+                isSigner: false,
+                isWritable: true,
+            },
+            {
+                pubkey: web3.SystemProgram.programId,
+                isSigner: false,
+                isWritable: false,
+            },
+        ],
+    });
+    transaction.add(instruction);
+    const transactionSign = await web3.sendAndConfirmTransaction(
+        connection,
+        transaction,
+        [publicUserKey]
+    );
+    console.log(
+        `Transaction: https://explorer.solana.com/tx/${transactionSign}?cluster=custom`
+    );
+    //fetch the data that has been sent to the network
+    //fetch accounts
+    const search = ""
+    let accounts = await connection.getProgramAccounts(
+        new web3.PublicKey(program_id),
+        {
+            dataSlice: {
+                offset: 2, length: 18
+            },
+            filters: search === "" ? [] : [
+                {
+                    memcmp: {
+                        offset: 6,
+                        bytes: bs58.encode(Buffer.from(search))
+                    }
+                }
+            ]
+        }
+    )
+    accounts.sort((a, b) => {
+        const lengthA = a.account.data.readUInt32LE(0)
+        const lengthB = b.account.data.readUInt32LE(0)
+        const dataA = a.account.data.slice(4, 4 + lengthA)
+        const dataB = b.account.data.slice(4, 4 + lengthB)
+        return dataA.compare(dataB)
+    })
+    accounts = accounts.map(account => account.pubkey)
+    //get the paginated public keys as per a required limit
+    let pageNo = 1;
+    let objectPerPage = 2;
+    // paginated data
+    const paginatedPublicKeys = accounts.slice(
+        (pageNo - 1) * objectPerPage,
+        pageNo * objectPerPage
+    )
+    //
+    let account_data = await connection.getMultipleAccountsInfo(paginatedPublicKeys);
+    //deserialize accounts to get the data
+    // account_data.map((acc_data,index)=>{
+    //     const {title, rating} = movieInstructionLayout.decode(acc_data.data)
+    //     console.log(title,rating)
+    //     return 0
+    // })
+    const borshAccountSchema = borsh.struct([
+        borsh.bool('is_initialized'),
+        borsh.u8('rating'),
+        borsh.str('title'),
+        borsh.str('description'),
+    ])
+    for (let i = 0; i < 2; i++) {
+        // retrieve the fields out of the account_data
+        const data = borshAccountSchema.decode(account_data[i].data)
+    }
 }
 
 main();
-
-
-
-
-
-// ==========CREATING HELLO WORLD WITH SOLANA - Part 1============
-// //program_id HZ3xkJisWVdfLGKidym566bDiAPbyogqr9pWi2yoQh8p
-// const dotenv = require("dotenv");
-// const web3 = require("@solana/web3.js");
-//
-// async function main() {
-//     let web3 = require("@solana/web3.js");
-//     let dotenv = require("dotenv")
-//     dotenv.config();
-//     const secret = JSON.parse(process.env.PRIVATE_KEY);
-//     const secret_key = Uint8Array.from(secret);
-//     // this is the generated keypair
-//     const publicKeyFromSecret = web3.Keypair.fromSecretKey(secret_key);
-//     // program id (created after uplodaing the program)
-//     const programId = new web3.PublicKey("9jXP33ikjYLzqnucmwT2X4syGTnxxoqTiHR9ecEJwZdo")
-//     const connection = new web3.Connection("http://localhost:8899");
-//     // transaction
-//     const transaction = new web3.Transaction();
-//     const instruction = new web3.TransactionInstruction({
-//         keys: [],
-//         programId
-//     })
-//     transaction.add(instruction);
-//     //generate transaction signature
-//     const transactionSign = await web3.sendAndConfirmTransaction(
-//         connection,
-//         transaction,
-//         [publicKeyFromSecret]
-//     );
-//     console.log(
-//         `Transaction: https://explorer.solana.com/tx/${transactionSign}?cluster=custom`
-//     );
-// }
-//
-// main()
--- src/lib.rs
+++ src/lib.rs
@@ -133,22 +133,3 @@ pub fn process_instruction(
         }
     }
 }
-
-
-// ==========CREATING HELLO WORLD WITH SOLANA - Part 1============
-// use solana_program::{
-//     account_info::AccountInfo,
-//     entrypoint,
-//     entrypoint::ProgramResult,
-//     pubkey::Pubkey,
-//     msg
-// };
-// pub fn process_instruction(
-//     program_id: &Pubkey,
-//     accounts: &[AccountInfo],
-//     instruction_data: &[u8]
-// )-> ProgramResult {
-//     msg!("Hello world! {:?}",program_id);
-//     return Ok(());
-// }
-// entrypoint!(process_instruction);
